85993a3ca4ccb0d92aaca6e294fa5ee765eccb56
JaveriaTariq973 <javeriat081@gmail.com>
2026-07-16 07:10:29 -0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string enc;
+        for(string x:strs){
+            enc+=to_string(x.length());
+            enc+='#';
+            enc+=x;
+        }
+        return enc;
+    }
+    vector<string> decode(string s) {
+        vector<string> strs;
+        int l=0;
+        int i=0;
+        while(i<s.length()){
+            int l=0;
+            while(s[i]!='#'){
+                l=l*10+(s[i]-'0');
+                i++;
+            }
+            i++;
+            string temp="";
+            for(int j=0;j<l;j++){
+                temp+=s[j];
+                i++;
+            }
+            strs.push_back(temp);
+        }
+        return strs;
+    }
+
+};
